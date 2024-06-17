3a9ceb5dbd87f527f268a8a8c9b475943ab23088
Mikkel Mork Hegnhoj <mikkel@fermyon.com>
2024-06-17 20:28:19 +0000

    init

---
+++ PluginWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace PluginWorld
+{
+    public class PluginWorldImpl : IPluginWorld
+    {
+        public static string Transform(string input)
+        {
+            return input.ToUpperInvariant();
+        }
+    }
+}
